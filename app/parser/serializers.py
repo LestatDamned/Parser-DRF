ef2af05350cefa733c87f0faaf5966c5fed95cd6
Maksim Bondar <lestatuk@gmail.com>
2024-09-28 16:51:24 +0700

    Исправления сериализатора юзера, исправлен docker-compose.yml

--- app/parser/serializers.py
+++ app/parser/serializers.py
@@ -26,8 +26,8 @@ class ArticleDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = (
-        "id", "search_id", "article_link", "title", "author_profile", "author", "author_rating", "content", "date",
-        "rating", "bookmarks", "comments")
+            "id", "search_id", "article_link", "title", "author_profile", "author", "author_rating", "content", "date",
+            "rating", "bookmarks", "comments")
 
     def create(self, validated_data):
         return Article.objects.create(**validated_data)
@@ -42,3 +42,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("username", "email", "password")
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'],
+                                        validated_data['email'], validated_data['password'])
+        return user
